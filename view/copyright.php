3f1dd37c18b8b9553678a17d412ea581414ab907
MrEoZMoDz <mreozmods12@gmail.com>
2019-04-03 12:36:03 -0300

    HTML

--- view/copyright.php
+++
@@ -1,3 +0,0 @@
-<div class="copyright">
-    &copy; 2013 - 2016 Thiengo [Calopsita] - Todos os direitos reservados
-</div>
--- view/top.php
+++
@@ -1,5 +0,0 @@
-<div class="header">
-    <a href="./" title="WebView User Sign Up">
-        WebView User Sign Up
-    </a>
-</div>
